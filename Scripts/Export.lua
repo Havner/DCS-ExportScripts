c00d5cef7a520fb971ec5d691ab31af2eabb247a
Lukasz Pawelczyk <havner@gmail.com>
2023-10-22 19:27:09 +0200

    Add Vled script

--- Scripts/Export.lua
+++ Scripts/Export.lua
@@ -1,2 +1,32 @@
 -- load the DCS ExportScript
 dofile(lfs.writedir()..[[Scripts\DCS-ExportScript\ExportScript.lua]])
+
+DE_LuaExportActivityNextEvent = LuaExportActivityNextEvent;
+DE_LuaExportStart = LuaExportStart;
+DE_LuaExportStop = LuaExportStop;
+
+
+-- load the Vled script
+dofile(lfs.writedir()..[[Scripts\vled\VledExport.lua]])
+
+VL_LuaExportActivityNextEvent = LuaExportActivityNextEvent;
+VL_LuaExportStart = LuaExportStart;
+VL_LuaExportStop = LuaExportStop;
+
+
+function LuaExportActivityNextEvent(t)
+    DE_LuaExportActivityNextEvent(t);
+    VL_LuaExportActivityNextEvent(t);
+
+    return t + ExportScript.Config.ExportInterval;
+end
+
+function LuaExportStart()
+    DE_LuaExportStart();
+    VL_LuaExportStart();
+end
+
+function LuaExportStop()
+    DE_LuaExportStop();
+    VL_LuaExportStop();
+end
